918d6f3966780c3fa34f628e49efeaa19cd68c84
Andrey Berezin <isbushcar@gmail.com>
2023-01-01 13:51:45 +0300

    Add base template

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import render_template, Flask
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
