68ba9c6f6fda3d075303f492be85d98be7ef9f7b
Andrey Berezin <isbushcar@gmail.com>
2023-01-02 13:01:01 +0300

    Add plug for checks functionality

--- database.sql
+++ database.sql
@@ -1,6 +1,15 @@
-CREATE TABLE urls
-(
+CREATE TABLE public.urls (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name text UNIQUE NOT NULL ,
+    created_at timestamp DEFAULT now()
+);
+
+CREATE TABLE public.url_checks (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id bigint REFERENCES public.urls (id) NOT NULL,
+    status_code int,
+    h1 text,
+    title text,
+    description text,
     created_at timestamp DEFAULT now()
 );
